a0e634810e7d72d4be856cbf96d76059fd7195db
alumno <alumno@DESKTOP-UE84JL7>
2022-11-14 20:32:51 +0100

    Primera

---
+++ src/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		//https://github.com/AgustinRF22/Prueba.git
+	}
+
+}
